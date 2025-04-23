19ea8ff57e603b096f46729598714423eded0bf8
barreiro-r <rodrigoasbarreiro@gmail.com>
2025-04-23 02:26:48 -0300

    add day 20

---
+++ 2025/dataset-sources/source_climate_wheater_surface_of_brazil.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -L -o ~/Downloads/hourly-weather-surface-brazil-southeast-region.zip\
+  https://www.kaggle.com/api/v1/datasets/download/PROPPG-PPG/hourly-weather-surface-brazil-southeast-region
